269349805edd3093b28ff28c237031c4836e316a
THOMAS <88090386+tomtricks@users.noreply.github.com>
2022-12-14 12:30:18 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -19,14 +19,14 @@ from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
+# from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('tryblog.urls')),
+#     path('',include('tryblog.url')),
     path('members/',include('django.contrib.auth.urls')),
-    path('members/',include('members.urls')),
+    path('members/',include('members.uls')),
 
     # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
